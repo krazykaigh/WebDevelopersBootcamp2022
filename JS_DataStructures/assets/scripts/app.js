abf11b550f59e59823e82f308d9a998df4ec2d05
Kaigh Taylor <coders@kai-deas.com>
2023-03-24 09:30:38 -0400

    wdbc | data structtures arrays

--- JS_DataStructures/assets/scripts/app.js
+++ JS_DataStructures/assets/scripts/app.js
@@ -1,1 +1,34 @@
 /*jshint esversion: 8 */
+
+// ARRAYS
+let planets = ['Moon','Venus','Earth','Mars','Jupiter'];
+// Remove Moon from the front, it's not a planet
+console.log(planets);
+planets.shift();
+console.log(planets);
+// Add Mercury to the front of planets
+planets.unshift('Mercury');
+console.log(planets);
+// Now add Saturn, Uranus and Neptune to the end of planets
+planets.push('Saturn', 'Uranus', 'Neptune');
+console.log(planets);
+
+const catsGroup1=['blue','wyatt','francesca'];
+const catsGroup2=['freddy','felix','max'];
+const allCats=catsGroup1.concat(catsGroup2);
+console.log(catsGroup1,catsGroup2,allCats);
+
+console.log(allCats[0]);
+console.log(allCats.indexOf("felix"));
+console.log(allCats.indexOf("wyatt"));
+
+console.log(allCats.reverse());
+console.log(allCats.reverse());
+
+let nums = [1,2,3];
+const numsCopy = nums;
+nums.push(4);
+console.log('numsCopy = ${numsCopy}');
+
+nums = ['a', 'b', 'c'];
+console.log(nums);
