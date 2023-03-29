ec8e0f07d8fadffc6debfcf1e893f5e56fc4ea96
Kaigh Taylor <coders@kai-deas.com>
2023-03-29 12:50:56 -0400

    wdbc | todos updated

--- TodoListProject/todos.js
+++ TodoListProject/todos.js
@@ -1,23 +1,30 @@
 let input = prompt('what would you like to do?');
 const todos = ['Collect Chicken Eggs', 'Clean Litter Box'];
 while (input !== 'quit' && input !== 'q') {
-    if (input === 'list') {
-        console.log('*****************')
+    if (input === 'list' || input === 'l') {
+        console.log('*****************');
         for (let i = 0; i < todos.length; i++) {
             console.log(`${i}: ${todos[i]}`);
         }
         console.log('*****************')
-    } else if (input === 'new') {
+    } else if (input === 'new' || input === 'n') {
         const newTodo = prompt('Ok, what is the new todo?');
-        todos.push(newTodo);
-        console.log(`${newTodo} added to the list!`)
-    } else if (input === 'delete') {
+        if (newTodo != "" && newTodo != undefined) {
+            todos.push(newTodo);
+            console.log(`${newTodo} added to the list!`);
+        }
+        else {
+            console.log('Entry was empty or undefined. Item not added.');
+        }
+    } else if (input === 'delete' or input === 'd') {
         const index = parseInt(prompt('Ok, enter an index to delete:'));
         if (!Number.isNaN(index)) {
+            if (index >= 0 && index < todos.length) {
             const deleted = todos.splice(index, 1);
             console.log(`Ok, deleted ${deleted[0]}`);
-        } else {
-            console.log('Unknown index')
+            } else {
+                console.log('Unknown index');
+            }
         }
     }
     input = prompt('what would you like to do?')
