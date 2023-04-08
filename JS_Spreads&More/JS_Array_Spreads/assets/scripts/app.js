d96187c4d62134e27fcd20aaf1987c532e0ee07a
Kaigh Taylor <coders@kai-deas.com>
2023-04-08 12:17:05 -0400

    wdbc | array spread

--- JS_Spreads&More/JS_Array_Spreads/assets/scripts/app.js
+++ JS_Spreads&More/JS_Array_Spreads/assets/scripts/app.js
@@ -14,4 +14,12 @@ let str = 'Hello';
 console.log(...str);
 
 const array1 = ['this','that','other'];
+console.log(`array1 = ${array1}`);
 const array2 = ['something', 'nothing', 'newton'];
+console.log(`array2 = ${array2}`);
+
+const array1Copy = [...array1];
+console.log(`array1Copy = ${array1Copy}`);
+
+const bothArraysPlus = [...array2,...array1,'scottie',...'Kaigh'];
+console.log(`bothArraysPlus = ${bothArraysPlus}`);
