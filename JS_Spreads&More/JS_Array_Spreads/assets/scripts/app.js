3090eb6340e33a59bef77038c6ce26a7d53fa350
Kaigh Taylor <coders@kai-deas.com>
2023-04-12 09:46:16 -0400

    wdbc | function Rest Params

--- JS_Spreads&More/JS_Array_Spreads/assets/scripts/app.js
+++ JS_Spreads&More/JS_Array_Spreads/assets/scripts/app.js
@@ -2,7 +2,7 @@
 
 
 
-const array1 = ['this','that','other'];
+const array1 = ['this', 'that', 'other'];
 console.log(`array1 = ${array1}`);
 const array2 = ['something', 'nothing', 'newton'];
 console.log(`array2 = ${array2}`);
@@ -10,5 +10,5 @@ console.log(`array2 = ${array2}`);
 const array1Copy = [...array1];
 console.log(`array1Copy = ${array1Copy}`);
 
-const bothArraysPlus = [...array2,...array1,'scottie',...'Kaigh'];
+const bothArraysPlus = [...array2, ...array1, 'scottie', ...'Kaigh'];
 console.log(`bothArraysPlus = ${bothArraysPlus}`);
--- JS_Spreads&More/JS_RestParams/assets/scripts/app.js
+++ JS_Spreads&More/JS_RestParams/assets/scripts/app.js
@@ -1,1 +1,32 @@
 /*jshint esversion: 8 */
+const sumAll = function () {
+  let total = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    total += arguments[i];
+  }
+  return total;
+};
+
+console.log(sumAll(8, 4, 3, 2)); // 17
+console.log(sumAll(-3, 4, 55)); // 56
+
+// Refresher
+// function sum() {
+//   return arguments.reduce((total, el) => total + el); //arguments is not an array
+// }
+
+// console.log(`sum = ${sum(1,23,4,53,336,2)}`);
+
+function sum(...nums) { // using the REST spread
+  return nums.reduce((total, el) => total + el); //arguments is not an array
+}
+
+console.log(`sum = ${sum(1, 23, 4, 53, 336, 2)}`);
+
+function raceResults(gold, silver, ...everyoneElse) {
+  //REST in functions collects the Rest of the arguments
+  console.log(`Gold Medal winner: ${gold}`);
+  console.log(`Silver Medal winner: ${silver}`);
+  console.log(`Participation Medal winners: ${everyoneElse}`);
+}
+console.log(raceResults('Eddie', 'Marie', 'Albert', 'Mikey', 'Spencer'));
