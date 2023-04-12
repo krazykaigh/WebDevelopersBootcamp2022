dbeecd1648f26dd2108aa1c3932717f8a6ad081e
Kaigh Taylor <coders@kai-deas.com>
2023-04-12 09:17:10 -0400

    wdbc | moved function spreads

--- JS_Spreads&More/JS_Array_Spreads/assets/scripts/app.js
+++ JS_Spreads&More/JS_Array_Spreads/assets/scripts/app.js
@@ -1,17 +1,6 @@
 /*jshint esversion: 8 */
 
-// way of entering array of numbers into function call
-const nums = [3,534,2,78,4,7,0,13,2,67,-6,0.34];
-console.log(Math.min(2,4,3));
-console.log(Math.max(3,534,2,78,4,7,0,13,2,67,-6,0.34));
-console.log(Math.max(nums));
 
-// Now with spread operator 
-console.log(`Max in the nums array: ${Math.max(...nums)}`);
-console.log(`Min in the nums array: ${Math.min(...nums)}`);
-
-let str = 'Hello';
-console.log(...str);
 
 const array1 = ['this','that','other'];
 console.log(`array1 = ${array1}`);
