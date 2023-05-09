1746f1a1f9d1bc94acac1f7997f23aefce30339f
Kaigh Taylor <coders@kai-deas.com>
2023-05-09 12:55:44 -0400

    wdbc | poikemon layout

--- DOM_Intro_CODE/Pokemon/app.js
+++ DOM_Intro_CODE/Pokemon/app.js
@@ -1,7 +1,7 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
 const container = document.querySelector('#container');
-const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
 
 for (let i = 1; i <= 151; i++) {
@@ -10,10 +10,10 @@ for (let i = 1; i <= 151; i++) {
     const label = document.createElement('span');
     label.innerText = `#${i}`;
     const newImg = document.createElement('img');
-    newImg.src = `${baseURL}${i}.png`
+    newImg.src = `${baseURL}${i}.png`;
 
 
-    pokemon.appendChild(newImg);
-    pokemon.appendChild(label);
-    container.appendChild(pokemon)
+    pokemon.append(newImg);
+    pokemon.append(label);
+    container.append(pokemon);
 }
